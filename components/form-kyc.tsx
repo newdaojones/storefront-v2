4114712e22aa19a00286a005066eeb46de275370
Devin Elliot <devinjelliot@gmail.com>
2023-06-12 20:51:08 -0600

    payments list started, onboard models started

---
+++ components/form-kyc.tsx
@@ -0,0 +1,156 @@
+"use client"
+
+import KycIndividual from '@/app/onboard/page';
+import { useState } from 'react';
+
+export type KycIndividual = {
+    firstName: string;
+    lastName: string;
+    phone: string;
+    email: string;
+    zipCode: string;
+    ssnOrTaxId: string;
+    dateOfBirth: string;
+    taxCountry: string;
+    address1: string;
+    address2: string;
+    city: string;
+    state: string;
+    docsAttachments: File | null;
+}
+
+// Beneficials are the same as 'individual's but they're associated with an entity and require an entity association
+export default function KycForms({ type }: { type: "individual" | "entity" | "beneficial" }) {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [phone, setPhone] = useState('');
+    const [email, setEmail] = useState('');
+    const [zipCode, setZipCode] = useState('');
+    const [ssnOrTaxId, setSsnOrTaxId] = useState('');
+    const [dateOfBirth, setDateOfBirth] = useState('');
+    const [taxCountry, setTaxCountry] = useState('');
+    const [address1, setAddress1] = useState('');
+    const [address2, setAddress2] = useState('');
+    const [city, setCity] = useState('');
+    const [state, setState] = useState('');
+    const [docsAttachments, setDocsAttachments] = useState<File | null>(null);
+
+    const handleSubmit = (e: { preventDefault: () => void; }) => {
+        e.preventDefault();
+
+        // Handle form data here
+        console.log(KycIndividual);
+    }
+
+    return (
+        <div className="h-screen flex items-center justify-center">
+            <div className="rounded shadow-md max-w-md w-full overflow-hidden p-6 bg-white mx-auto">
+                <form onSubmit={handleSubmit} className="grid grid-cols-1 gap-4">
+                    <div className="grid grid-cols-2 gap-4">
+                        <input
+                            type="text"
+                            placeholder="First Name"
+                            value={firstName}
+                            onChange={(e) => setFirstName(e.target.value)}
+                        />
+                        <input
+                            type="text"
+                            placeholder="Last Name"
+                            value={lastName}
+                            onChange={(e) => setLastName(e.target.value)}
+                        />
+                    </div>
+
+                    <input
+                        type="text"
+                        placeholder="Phone Number"
+                        value={phone}
+                        onChange={(e) => setPhone(e.target.value)}
+                    />
+
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+
+                    <div className="grid grid-cols-3 gap-4">
+                        <input
+                            type="text"
+                            placeholder="SSN or Tax ID"
+                            value={ssnOrTaxId}
+                            onChange={(e) => setSsnOrTaxId(e.target.value)}
+                        />
+                        <input
+                            type="date"
+                            placeholder="Date of Birth"
+                            value={dateOfBirth}
+                            onChange={(e) => setDateOfBirth(e.target.value)}
+                        />
+                        <input
+                            type="text"
+                            placeholder="Tax Country"
+                            value={taxCountry}
+                            onChange={(e) => setTaxCountry(e.target.value)}
+                        />
+                    </div>
+
+                    <hr className="my-4" />
+
+                    <input
+                        type="text"
+                        placeholder="Address 1"
+                        value={address1}
+                        onChange={(e) => setAddress1(e.target.value)}
+                        required
+                    />
+                    <input
+                        type="text"
+                        placeholder="Address 2"
+                        value={address2}
+                        onChange={(e) => setAddress2(e.target.value)}
+                    />
+
+                    <div className="grid grid-cols-3 gap-4">
+                        <input
+                            type="text"
+                            placeholder="City"
+                            value={city}
+                            onChange={(e) => setCity(e.target.value)}
+                            required
+                        />
+                        <input
+                            type="text"
+                            placeholder="State"
+                            value={state}
+                            onChange={(e) => setState(e.target.value)}
+                            required
+                        />
+                        <input
+                            type="text"
+                            placeholder="Zip Code"
+                            value={zipCode}
+                            onChange={(e) => setZipCode(e.target.value)}
+                            required
+                        />
+                    </div>
+
+                    <input
+                        type="file"
+                        placeholder="Attachments"
+                        onChange={(e) => {
+                            if (e.target.files) {
+                                setDocsAttachments(e.target.files[0]);
+                            }
+                        }}
+                    />
+
+                    <button type="submit" className="w-full py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        Submit
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+}
--- components/form.tsx
+++ components/form.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { useState } from "react";
-import { signIn } from "next-auth/react";
 import LoadingDots from "@/components/loading-dots";
-import toast from "react-hot-toast";
+import { signIn } from "next-auth/react";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
+import toast from "react-hot-toast";
 
 export default function Form({ type }: { type: "login" | "register" }) {
   const [loading, setLoading] = useState(false);
@@ -46,7 +46,7 @@ export default function Form({ type }: { type: "login" | "register" }) {
             if (res.status === 200) {
               toast.success("Account created! Redirecting to login...");
               setTimeout(() => {
-                router.push("/login");
+                router.push("/onboard");
               }, 2000);
             } else {
               const { error } = await res.json();
@@ -91,11 +91,10 @@ export default function Form({ type }: { type: "login" | "register" }) {
       </div>
       <button
         disabled={loading}
-        className={`${
-          loading
+        className={`${loading
             ? "cursor-not-allowed border-gray-200 bg-gray-100"
             : "border-black bg-black text-white hover:bg-white hover:text-black"
-        } flex h-10 w-full items-center justify-center rounded-md border text-sm transition-all focus:outline-none`}
+          } flex h-10 w-full items-center justify-center rounded-md border text-sm transition-all focus:outline-none`}
       >
         {loading ? (
           <LoadingDots color="#808080" />
