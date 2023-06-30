57c2d385b31c7f76ad80023637f5501ce18db5eb
Devin Elliot <devinjelliot@gmail.com>
2023-06-30 11:17:02 -0600

    cleaned up button styling, refactored to css module

--- components/order/cancel-submit.tsx
+++ components/order/cancel-submit.tsx
@@ -1,3 +1,4 @@
+import Tooltip from '../tooltips/tooltip';
 import styles from './order.module.css';
 
 type ButtonsProps = {
@@ -9,19 +10,23 @@ type ButtonsProps = {
 export default function Buttons({ onSubmit, onCancel, isOrderValid }: ButtonsProps) {
     return (
         <div className="flex flex-row space-x-4">
-            <button
-                onClick={onCancel}
-                className={styles.cancelButton}
-            >
-                Cancel
-            </button>
-            <button
-                onClick={onSubmit}
-                disabled={!isOrderValid}
-                className={isOrderValid ? styles.submitButton : styles.submitButtonDisabled}
-            >
-                Submit
-            </button>
+            <Tooltip text={'Cancel will clear the form'}>
+                <button
+                    onClick={onCancel}
+                    className={styles.cancelButton}
+                >
+                    Cancel
+                </button>
+            </Tooltip>
+            <Tooltip text={'Fill out form before you can submit'}>
+                <button
+                    onClick={onSubmit}
+                    disabled={!isOrderValid}
+                    className={isOrderValid ? styles.submitButton : styles.submitButtonDisabled}
+                >
+                    Submit
+                </button>
+            </Tooltip>
         </div>
     );
 }
