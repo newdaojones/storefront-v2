4a10150dc4481b8a3ac298d62af0e8613e2de133
Devin Elliot <devinjelliot@gmail.com>
2023-07-01 17:28:00 -0600

    wip: order api

--- components/settings/address.tsx
+++ components/settings/address.tsx
@@ -1,7 +1,5 @@
 
 // app/SettlementAddress.tsx
-import { useEffect, useState } from 'react';
-import { fetchSettlementAddress } from '../../app/api/[merchant]/route';
 import styles from './settings.module.css';
 
 interface SettlementAddressProps {
@@ -11,21 +9,12 @@ interface SettlementAddressProps {
 const SettlementAddress: React.FC<SettlementAddressProps> = ({
     params: { settlementAddress },
 }) => {
-    const [address, setAddress] = useState<string | null>(null);
 
-    useEffect(() => {
-        async function fetchData() {
-            const data = await fetchSettlementAddress(settlementAddress);
-            setAddress(data?.toString() || null);
-        }
-
-        fetchData();
-    }, [settlementAddress]);
 
     return (
         <div className={styles.settlementAddress}>
             <div className={styles.label}>Settlement Address</div>
-            <div className={styles.boxSettlementAddress}>{address}</div>
+            <div className={styles.boxSettlementAddress}>0xdeedbeef</div>
         </div>
     );
 };
