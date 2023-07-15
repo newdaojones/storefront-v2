74cc38561c3befcae2503b5cd6371850395248b2
Devin Elliot <devinjelliot@gmail.com>
2023-07-15 07:07:28 -0600

    accept walletconnect additions and merge with main branch

--- app/api/payments/route.ts
+++ app/api/payments/route.ts
@@ -1,15 +1,13 @@
-// 
 import prisma from "@/lib/prisma";
 import { NextResponse } from "next/server";
-
 
 export async function POST(req: Request) {
     const { from, to } = await req.json();
     const payments = await prisma.payment.findMany({
         where: {
             createdAt: {
-                gte: new Date(from),
-                lte: new Date(to),
+                gte: new Date(from), //gte = greater than or equal to
+                lte: new Date(to), //lte = less than or equal to
             }
         },
         include: {
