b2a304531cb63de3dda5423f299a0c8e988ff4ce
Devin Elliot <devinjelliot@gmail.com>
2023-07-30 21:56:36 -0400

    refactore bridge kyc type out to types file

---
+++ components/forms/bridge.d.ts
@@ -0,0 +1,17 @@
+export type BridgeKycFormData = {
+    email: string;
+    password: string;
+    companyName: string;
+    firstName: string;
+    lastName: string;
+    phoneNumber: string;
+    ssn: string;
+    dob: string;
+    streetAddress: string;
+    streetAddress2: string;
+    city: string;
+    state: string;
+    postalCode: string;
+    country: string;
+    signedAgreementId: string;
+}
--- components/forms/kyc-bridge.tsx
+++ components/forms/kyc-bridge.tsx
@@ -1,25 +1,9 @@
 "use client"
 
 import React, { useState } from 'react';
+import { BridgeKycFormData } from './bridge';
 import styles from './form.module.css';
 
-type BridgeKycFormData = {
-    email: string;
-    password: string;
-    companyName: string;
-    firstName: string;
-    lastName: string;
-    phoneNumber: string;
-    ssn: string;
-    dob: string;
-    streetAddress: string;
-    streetAddress2: string;
-    city: string;
-    state: string;
-    postalCode: string;
-    country: string;
-    signedAgreementId: string;
-}
 
 type BridgeKycFormErrors = { [K in keyof BridgeKycFormData]?: string };
 
